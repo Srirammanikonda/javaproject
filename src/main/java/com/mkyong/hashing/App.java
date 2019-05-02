8c5917127a184e306dff8d52c2084d1ed67d6e01
Srirammanikonda <mailtosrirammanikonda@gmail.com>
2019-05-02 03:57:48 -0700

    Initial commit

---
+++ src/main/java/com/mkyong/hashing/App.java
@@ -0,0 +1,25 @@
+package com.mkyong.hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public boolean isPrime(int n){
+
+	boolean prime=true;
+	for(int i =2;i<=n;i++){
+	    if(n%i==0){
+	prime = false;
+	break;
+}
+}
+return prime;	
+}
+}
